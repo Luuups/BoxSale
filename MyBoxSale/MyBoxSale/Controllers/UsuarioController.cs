ea44d49aa4ea094f4ed8056ca442be2916b3ba92
Edrei Javier Bastar Sarao <basae_01@hotmail.com>
2016-12-03 13:36:04 -0600

    modulo para nuevo usuario aplicado

--- MyBoxSale/MyBoxSale/Controllers/UsuarioController.cs
+++ MyBoxSale/MyBoxSale/Controllers/UsuarioController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MyBoxSale.Core;
+using MyBoxSale.Filters;
 using MyBoxSale.Models;
 using MyBoxSale.Models.Local;
+using WebMatrix.WebData;
 
 namespace MyBoxSale.Controllers
 {
@@ -21,19 +24,19 @@ namespace MyBoxSale.Controllers
             {
                 using (Entities)
                 {
-                    var Usuarios = (from usr in Entities.USUARIO
-                                    
-                                   orderby usr.Apellidos
-                                   select new UsuarioView
-                                   {
-                                       Id=usr.Id,
-                                       Nombre=usr.Nombre,
-                                       Apellidos=usr.Apellidos,
-                                       Telefono=usr.Telefono,
-                                       NombreUsuario=usr.NombreUsuario,
-                                       Empresa=usr.EMPRESA,
-                                       Activo=usr.Activo
-                                   }).ToList();
+                    var Usuarios = (from usr in Entities.USUARIO.Include("USUARIOROLES").Include("USUARIOROLES.ROLES")
+                                    where usr.USUARIOROLES.Any(x => x.ROLES.Nombre != "Admin")
+                                    select new UsuarioView
+                                    {
+                                        Id = usr.Id,
+                                        Nombre = usr.Nombre,
+                                        Apellidos = usr.Apellidos,
+                                        Telefono = usr.Telefono,
+                                        NombreUsuario = usr.NombreUsuario,
+                                        Empresa = usr.EMPRESA,
+                                        Activo = usr.Activo
+                                    }).ToList();
+
                     return View(Usuarios);
                 }
             }
@@ -113,19 +116,71 @@ namespace MyBoxSale.Controllers
 
         [ValidateAntiForgeryToken]
         [HttpPost]
-        public ActionResult Edit(USUARIO id)
+        public ActionResult Edit(USUARIO _usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (Entities)
+                    {
+                        var usuario = Entities.USUARIO.Find(_usuario.Id);
+                        usuario.Nombre = _usuario.Nombre;
+                        usuario.Apellidos = _usuario.Apellidos;
+                        usuario.Direccion = _usuario.Direccion;
+                        usuario.Telefono = _usuario.Telefono;
+                        Entities.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult NewUser()
         {
             try
             {
                 using (Entities)
                 {
-                    return View();
+                    ViewBag.Empresa = Entities.EMPRESA.FirstOrDefault();
                 }
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [InitializeSimpleMembership]
+        public ActionResult NewUser(USUARIO _newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (Entities)
+                    {
+                        _newUser.EmpresaId = Entities.EMPRESA.FirstOrDefault().Id;
+                        Entities.USUARIO.Add(_newUser);
+                        Entities.SaveChanges();
+                        Roles.AddUsersToRoles(new[] { _newUser.NombreUsuario }, new[] { "Cajero" });
+                        WebSecurity.CreateAccount(_newUser.NombreUsuario, _newUser.Password);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+            return View();
         }
     }
 }
