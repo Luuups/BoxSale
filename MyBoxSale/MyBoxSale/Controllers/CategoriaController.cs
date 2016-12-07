09a75919b9f77770bc925982ea483298f75a7f03
Edrei Javier Bastar Sarao <basae_01@hotmail.com>
2016-12-07 13:37:42 -0600

    categoria y unida de medida CRUD

---
+++ MyBoxSale/MyBoxSale/Controllers/CategoriaController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyBoxSale.Core;
+using MyBoxSale.Models.Local;
+using MyBoxSale.Models;
+
+namespace MyBoxSale.Controllers
+{
+    [Authorize(Roles="Admin")]
+    public class CategoriaController : BoxSaleController
+    {
+        //
+        // GET: /Categoria/
+
+        public ActionResult Index()
+        {
+            using (Entities)
+            {
+                var Categorias = (from cat in Entities.CATEGORIA
+                                 orderby cat.Nombre
+                                 select new CategoriaView
+                                 {
+                                     Id=cat.Id,
+                                     Nombre=cat.Nombre,
+                                     Activo=cat.Activo
+                                 }).ToList();
+                
+                return View(Categorias);
+            }
+        }
+
+        public ActionResult NuevaCategoria()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NuevaCategoria(CATEGORIA _categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (Entities)
+                    {
+                        _categoria.Activo = true;
+                        Entities.CATEGORIA.Add(_categoria);
+                        Entities.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Editar(int id)
+        {
+            try
+            {
+                using (Entities)
+                {
+                    var categoria = Entities.CATEGORIA.Find(id);
+                    return View(categoria);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(CATEGORIA _categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (Entities)
+                    {
+                        var categoria = Entities.CATEGORIA.Find(_categoria.Id);
+                        categoria.Nombre = _categoria.Nombre;
+                        Entities.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            try
+            {
+                using(Entities)
+                {
+                    var categoria = Entities.CATEGORIA.Find(id);
+                    Entities.CATEGORIA.Remove(categoria);
+                    Entities.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+        }
+
+        public ActionResult Desactivar(int id)
+        {
+            try
+            {
+                using (Entities)
+                {
+                    var cat = Entities.CATEGORIA.Find(id);
+                    if (cat.Activo.HasValue && cat.Activo.Value)
+                        cat.Activo = false;
+                    else
+                        cat.Activo = true;
+                    Entities.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ MyBoxSale/MyBoxSale/Models/Local/UnidadView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBoxSale.Models.Local
+{
+    public class UnidadView
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Abreviado { get; set; }
+        public bool? Activo { get; set; }
+    }
+}
